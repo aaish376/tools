068473e28e84f98891a1833d7d5293daa2a8ef6e
aaish376 <bcsf21m.pucit@gmail.com>
2025-07-22 00:45:53 +0500

    unet

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,12 @@
 from rembg import remove
 import io
+import os
 
 def remove_background(input_bytes):
-    result = remove(input_bytes)
+    # Get path to local model file
+    model_dir = os.path.join(os.path.dirname(__file__), 'u2net')
+    os.environ['U2NET_HOME'] = model_dir  # prevent rembg from downloading
+    
+    # Remove background using the locally stored u2net.onnx
+    result = remove(input_bytes, model_name='u2net')
     return io.BytesIO(result)
