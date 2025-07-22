bcbf9994bdb59afc736dcf54a7df616b1003fdac
aaish376 <bcsf21m.pucit@gmail.com>
2025-07-22 23:54:49 +0500

    env order change

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,12 @@
+import os
+os.environ["REMBG_SESSION_CACHE_HOME"] = "/opt/render/project/.cache" 
+
 from rembg import remove
 import io
-import os
 
 def remove_background(input_bytes):
     
     print("\n\nin utils removebg\n\n")
-    os.environ["REMBG_SESSION_CACHE_HOME"] = "/opt/render/project/.cache" 
     
     print("\n\nafter env and calling remove fn\n\n")
     # Remove background using the locally stored u2net.onnx
