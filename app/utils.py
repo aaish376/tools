e38a789023a8a20a858d708d45c9b05327ca2088
aaish376 <bcsf21m.pucit@gmail.com>
2025-07-22 22:47:40 +0500

    u2net update

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ import io
 import os
 
 def remove_background(input_bytes):
-    os.environ["REMBG_SESSION_CACHE_HOME"] = "/opt/render/project/.cache"
+    os.environ["REMBG_SESSION_CACHE_HOME"] = "/opt/render/project/.cache" 
     
     # Remove background using the locally stored u2net.onnx
     result =  remove(input_bytes)
